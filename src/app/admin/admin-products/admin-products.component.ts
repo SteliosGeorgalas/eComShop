2d9d3254d65166c9c84190bfc7b2627a7ed860c5
steliosg <stelios_geo1@yahoo.gr>
2022-09-06 11:02:03 +0300

    Finalize Admin Products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product';
-import { ProductsComponent } from 'src/app/products/products.component';
+// import { ProductsComponent } from 'src/app/products/products.component';
+// import { settings } from 'cluster';
 
 @Component({
   selector: 'app-admin-products',
@@ -13,10 +14,16 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products!: Product[];
   filterProducts!: Product[];
   subscription: Subscription;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
 
   constructor(private productService: ProductService) {
     this.subscription = productService.getAll<Product>()
-      .subscribe(products => this.filterProducts = this.products = products);
+      .subscribe(products => {
+        this.filterProducts = this.products = products;
+        this.dtTrigger.next(null);
+        
+      });
   }
 
   filterByTitle(query: string) {
@@ -26,10 +33,30 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      searching: false,
+      pagingType: 'full_numbers',
+      order: [0, 'asc'],
+      columnDefs: [{
+        targets: [2],//editRow
+        orderable: false
+      }]
+    }
+    // $.fn['dataTable'].ext.search.push((settings, data, dataIndex) => {
+    //   const id = parseFloat(data[0]) || 0;
+    //   if ((isNaN(this.min) && isNaN(this.max)) ||
+    //   (isNaN(this.min) && id <= this.max) ||
+    //   (this.min <= id && isNaN(this.max)) ||
+    //   (this.min <= id && id<= this.max) {
+    //     return true;
+    //   }
+    //   return false;
+    // })
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+    this.dtTrigger.unsubscribe();
   }
 
 
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -76,10 +76,16 @@ export class AuthService {
     return this.afAuth
       .signInWithPopup(provider)
       .then((result) => {
+        if (!result.user) return;
+
         if (result.user) {
           this.userService.save(result.user);
           // this.SetUserData(result.user); //For FireStore
         }
+        // let returnUrl = localStorage.getItem('returnUrl');
+        // if (!returnurl) return;
+        //   localStorage.removeItem('returnUrl');
+        //router.navigateByUrl(returnUrl);
         this.router.navigateByUrl(this.route.snapshot.queryParamMap.get('returnUrl') || '/');
       })
       .catch((error) => {
